3f958f2cdad5d62950d6e048d385f28188b2c374
Johan.Alger <johan.alger@gmail.com>
2022-03-22 14:17:20 +0100

    Correction commit suite erreur

--- js/script.js
+++ js/script.js
@@ -11,15 +11,19 @@ function init() {
   //Filtre des recettes dans l'onglet recherche
   let searchText = document.querySelector(".form-control").value;
   searchText = searchText.length > 2 ? searchText : "";
-  let recipesFiltred = "";
+  // let recipesFiltred = "";
+
+  let recipesFiltred = [];
   if (searchText) {
-    recipesFiltred = recipes.filter((recipe) => {
-      return (
+    for (const recipe of recipesFiltred) {
+      if (
         clearText(recipe.name).includes(clearText(searchText)) ||
         recipe.ingredients.some((ingredient) => clearText(ingredient.ingredient).includes(clearText(searchText))) ||
         recipe.description.includes(clearText(searchText))
-      );
-    });
+      ) {
+        recipesFiltred.push(recipe);
+      }
+    }
   } else {
     recipesFiltred = recipes;
   }
@@ -27,29 +31,29 @@ function init() {
   //Filtres des recettes par ingrédients (dropdown)
   const badgeIngredientList = badgeListFactory(".ingredientsBadges");
   if (badgeIngredientList.length > 0) {
-    badgeIngredientList.forEach((badge) => {
+    for (const badge of badgeIngredientList) {
       recipesFiltred = recipesFiltred.filter((recipe) => {
         return recipe.ingredients.some((ingredient) => clearText(ingredient.ingredient).includes(clearText(badge)));
       });
-    });
+    }
   }
   //Filtres des recettes par appareils (dropdown)
   const badgeAppareilsList = badgeListFactory(".appareilsBadges");
   if (badgeAppareilsList.length > 0) {
-    badgeAppareilsList.forEach((badge) => {
+    for (const badge of badgeAppareilsList) {
       recipesFiltred = recipesFiltred.filter((recipe) => {
         return clearText(recipe.appliance).includes(clearText(badge));
       });
-    });
+    }
   }
   //Filtres des recettes par ustensiles (dropdown)
   const badgeUstensilesList = badgeListFactory(".ustensilesBadges");
   if (badgeUstensilesList.length > 0) {
-    badgeUstensilesList.forEach((badge) => {
+    for (const badge of badgeUstensilesList) {
       recipesFiltred = recipesFiltred.filter((recipe) => {
         return recipe.ustensils.some((ustensile) => clearText(ustensile).includes(clearText(badge)));
       });
-    });
+    }
   }
   //Création des cards avec les recettes filtrées et les afficher
   cardsFactory(recipesFiltred);
@@ -59,49 +63,46 @@ function init() {
   appareilsList = [];
   ustensilesList = [];
   //Création des listes des dropdowns avec les recettes filtrées
-  recipesFiltred.forEach((recipe) => {
-    recipe.ingredients.forEach((ingredient) => {
+  for (const recipe of recipesFiltred) {
+    /* A loop that iterates over the elements of the array. */
+    for (const ingredient of recipe.ingredients) {
       const ingredientName = ingredient.ingredient;
       ingredientsList.includes(ingredientName) || badgeIngredientList.includes(ingredientName)
         ? null
         : ingredientsList.push(ingredientName);
-    });
+    }
 
     const appareilName = recipe.appliance;
     appareilsList.includes(appareilName) || badgeAppareilsList.includes(appareilName)
       ? null
       : appareilsList.push(appareilName);
 
-    recipe.ustensils.forEach((ustensil) => {
+    for (const ustensil of recipe.ustensils) {
       const ustensilName = ustensil;
       ustensilesList.includes(ustensilName) || badgeUstensilesList.includes(ustensilName)
         ? null
         : ustensilesList.push(ustensilName);
-    });
-  });
+    }
+  }
 
   //Ajoute les options des dropdowns
+  /* It creates a listener for each dropdown. */
   datalist(ingredientsList, "#ingredients", ".ingredientsList");
   datalist(appareilsList, "#appareils", ".appareilsList");
   datalist(ustensilesList, "#ustensiles", ".ustensilesList");
+
   /**
-   * Fonction qui crée un écouteur d'événement pour chaque dropdown
-   */
-  /**
-   * *This function takes in a list of elements, a target class, and a color, and adds a click event to
-   * each element in the list.
-   * It then uses the click event to call the badgeFactory function.*
-   * @param sourceElements - The element that contains the list of elements that will be used to generate
-   * the badges.
-   * @param badgeTargetClass - The CSS class of the element that will be the badge.
+   * It creates badges for each element of the dropdown.
+   * @param sourceElements - The element that contains the list of elements to be clicked.
+   * @param badgeTargetClass - The class of the badge that will be created.
    * @param color - The color of the badge.
    */
   function onElementClick(sourceElements, badgeTargetClass, color) {
-    document.querySelectorAll(sourceElements + " li").forEach((element) =>
+    for (const element of document.querySelectorAll(sourceElements + " li")) {
       element.addEventListener("click", (e) => {
         badgeFactory(badgeTargetClass, color, e.target.innerText, e.target.attributes["value"].value);
-      })
-    );
+      });
+    }
   }
   onElementClick("#ingredients", ".ingredientsBadges", "primaryColor");
   onElementClick("#appareils", ".appareilsBadges", "successColor");
@@ -113,12 +114,6 @@ document.querySelector(".form-control").addEventListener("input", (e) => {
   init();
 });
 
-/**
- * It creates a dropdown menu and binds it to a badge.
- * @param dropdownClass - The class of the dropdown element.
- * @param badgeTargetClass - The class of the element that will be updated with the new badge.
- * @param color - The color of the badge.
- */
 function onDropdownChange(dropdownClass, badgeTargetClass, color) {
   document.querySelector(dropdownClass).addEventListener("change", (e) => {
     badgeFactory(badgeTargetClass, color, e.target.value, e.target.value);
@@ -173,7 +168,6 @@ function launchDropdownAppareils() {
 }
 dropdownDownAppareils.addEventListener("click", (e) => {
   launchDropdownAppareils();
-  console.log("click");
 });
 function upDropDownAppareils() {
   openedBtnAppareils.style.display = "none";
